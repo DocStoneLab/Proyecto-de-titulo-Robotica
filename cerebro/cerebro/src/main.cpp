15ac0dd2b75fd9402070f722a2b895644afbbb6f
Joaquin B. <joako.jb12@gmail.com>
2026-05-25 00:03:05 -0400

    Agregue medicion con el sensor de ultrasonido
    Ahora el ultrasonida entrega la distancia y si esta a menos de 30cm dice
    que no se puede avanzar

--- cerebro/cerebro/src/main.cpp
+++ cerebro/cerebro/src/main.cpp
@@ -1,18 +1,66 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
+// Definición de los pines
+const int trigPin = 9;
+const int echoPin = 10;
+const int linePin = 8;
+
+// Variables para calcular la distancia
+long duracion;
+int distancia;
+bool Linea;
+
+int medir_distancia(){
+  // 1. Limpiar el pin Trig
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  
+  // 2. Emitir un pulso ultrasónico de 10 microsegundos
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  
+  // 3. Leer el tiempo que tarda el eco en regresar
+  duracion = pulseIn(echoPin, HIGH);
+  
+  // 4. Calcular la distancia (la velocidad del sonido es aprox. 340 m/s o 0.034 cm/µs)
+  // Como el sonido va y vuelve, dividimos el tiempo entre 2.
+  distancia = duracion * 0.034 / 2;
+  
+  // 5. Mostrar el resultado en el Monitor Serial
+  //Serial.print("Distancia: ");
+  //Serial.print(distancia);
+  //Serial.println(" cm");
+  
+  delay(500); // Espera medio segundo antes de la siguiente medición
+
+  return distancia;
+}
+
+void avanzar(int distancia){
+
+  if (distancia < 30)
+  {
+    Serial.println("No avanzar");
+  }
+  else
+  {
+    Serial.println("avanzar");
+  }
+  
+}
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  pinMode(trigPin, OUTPUT); // El pin Trig emite el pulso
+  pinMode(echoPin, INPUT);  // El pin Echo recibe el rebote
+  pinMode(linePin, INPUT);  // EL pin de el Sensor de linea
+  Serial.begin(9600);       // Inicializa la comunicación serial
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
+  Serial.println(medir_distancia());
+  avanzar(medir_distancia());
+  Linea = digitalRead(linePin);
+  Serial.println(Linea);
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
 }
